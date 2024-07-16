2adb9107132578e4fc7779140f372a9cd21bd136
Alexander Maiboroda <122598985+Gre4ko@users.noreply.github.com>
2024-07-16 15:59:15 +0300

    Update GeometryMatrix.java

--- src/main/java/me/drayff/binogic/GeometryMatrix.java
+++ src/main/java/me/drayff/binogic/GeometryMatrix.java
@@ -3,12 +3,15 @@ package me.drayff.binogic;
 public class GeometryMatrix {
 
     //creating a rectangle using coordinates of points that are the ends of the diagonal
-    public void rectangle(BinMatrix matrix, int x0, int x1, int y0, int y1) {
+    public BinMatrix rectangle(BinMatrix matrix, int x0, int x1, int y0, int y1) {
+        new_matrix = matrix.getMatrix();
+        new2matrix = new BinMatrix();
         for (int i = x0; i < x1; i++) {
             for (int j = y0; j < y1; j++) {
-                if (matrix[i][j] == 0)
-                    matrix[i][j] = 1;
+                if (new_matrix[i][j] == 0)
+                    new_matrix[i][j] = 1;
             }
         }
+        return new2matrix;
     }
 }
