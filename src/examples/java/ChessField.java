bf9182ab22c00df284e279bfa7e4430d10ca854f
Alexander Maiboroda <122598985+Gre4ko@users.noreply.github.com>
2024-07-16 16:09:17 +0300

    Update ChessField.java

--- src/examples/java/ChessField.java
+++ src/examples/java/ChessField.java
@@ -24,8 +24,8 @@ class ChessField {
 
         for(int j = 0; j < matrixLength; j+=2) {
             for(int i = 0; i < matrixLength; i+=2) {
-                objectMatrix.rectangle(i,i+1,j,j+1);
-                objectMatrix.rectangle(i+1,i+2,j+1,j+2);
+                objectMatrix = GeometryMatrix.rectangle(objectMatrix, i,i+1,j,j+1);
+                objectMatrix = GeometryMatrix.rectangle(objectMatrix, i+1,i+2,j+1,j+2);
             }
         }
 
